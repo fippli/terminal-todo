34244aad4691de62eeaa7dd273c3446bfa4cfa0b
Filip Johansson <filip.johansson@protonmail.com>
2019-12-08 14:13:14 +0100

    Updates a few comments

--- read_tasks.sh
+++ read_tasks.sh
@@ -5,18 +5,21 @@ padding="       "
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-# Count the lines ins the file
 lines=$( wc -l < "$todo_task_file" )
 i=0
 echo ""
+
 while [[ $i -lt $((lines)) ]]; do
   i=$((i+1))
   task_line="$( sed "${i}q;d" "$todo_task_file" )"
   task="$(echo $task_line| cut -d']' -f 2)"
   task_status="$(echo $task_line| cut -d']' -f 1)"
+  
   if [ "$task_status" = "[>" ]; then
+    # The selected task should be bold
     printf "${bold}${padding}%s${normal}\n\n" "$task"  
   else
+    # All other tasks should be normal
     printf "${normal}${padding}%s\n\n" "$task"
   fi
 done
