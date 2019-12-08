13ce9c5caaaa0bdb27357f3ce7269daea9f3d084
Filip Johansson <filip.johansson@protonmail.com>
2019-12-08 12:51:06 +0100

    Arrow up seems to work

--- change_selection_down.sh
+++ change_selection_down.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
 
-# Get the currently selected row index
+#!/usr/bin/env bash
 
-# Increase the index with one, min 0
+todo_task_file=$1
+i=0
+lines=$( wc -l < "$todo_task_file" )
 
-# write [>] to that row and [] to all others
+while [[ $i -lt $((lines)) ]]; do
+  line_number=$((i+1))
+  task_line="$( sed "${line_number}q;d" "$todo_task_file" )"
+  task_status="$(echo $task_line| cut -d']' -f 1)"
+  task_text="$(echo $task_line| cut -d']' -f 2)"
 
-echo "Decreasing"
+  if [[ "$task_status" == "[>" ]] && ((line_number < lines)); then
+    echo "Found the line ${line_number} "
+    # Unselect the selected line
+    unselect_task_update="[<]${task_text}"
+    sed -i '' "${line_number} s/.*/${unselect_task_update}/" "$todo_task_file"
+    
+    # Select the previous line
+    previous_line_number=$i
+    previous_task_line="$( sed "${previous_line_number}q;d" "$todo_task_file" )"
+    previous_task_text="$(echo $previous_task_line| cut -d']' -f 2)"
+    select_task_update="[>]${previous_task_text}"
+    sed -i '' "${previous_line_number} s/.*/${select_task_update}/" "$todo_task_file"
+  fi
+
+  i=$((i+1))
+done
--- change_selection_up.sh
+++ change_selection_up.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 
 todo_task_file=$1
-line_number=0
+i=0
 lines=$( wc -l < "$todo_task_file" )
 
-while [[ $line_number -lt $((lines)) ]]; do
-  line_number=$((line_number+1))
+while [[ $i -lt $((lines)) ]]; do
+  line_number=$((i+1))
   task_line="$( sed "${line_number}q;d" "$todo_task_file" )"
   task_status="$(echo $task_line| cut -d']' -f 1)"
   task_text="$(echo $task_line| cut -d']' -f 2)"
 
-  if [[ "$task_status" == "[>" && $((line_number > 0)) ]]; then
+  if [[ "$task_status" == "[>" ]] && ((line_number > 1)); then
     # Unselect the selected line
     unselect_task_update="[<]${task_text}"
     sed -i '' "${line_number} s/.*/${unselect_task_update}/" "$todo_task_file"
     
     # Select the previous line
-    previous_line_number=$((line_number-1))
-    previous_task_line"$( sed "${previous_line_number}q;d" "$todo_task_file" )"
-    select_task_update="[>]${task_text}"
+    previous_line_number=$i
+    previous_task_line="$( sed "${previous_line_number}q;d" "$todo_task_file" )"
+    previous_task_text="$(echo $previous_task_line| cut -d']' -f 2)"
+    select_task_update="[>]${previous_task_text}"
     sed -i '' "${previous_line_number} s/.*/${select_task_update}/" "$todo_task_file"
   fi
+
+  i=$((i+1))
 done
