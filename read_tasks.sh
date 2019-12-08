ed38d099ba4a86372a3c4219af607ecf95aa1cbf
Filip Johansson <filip.johansson@protonmail.com>
2019-12-08 18:47:56 +0100

    Adds color to selected item

--- read_tasks.sh
+++ read_tasks.sh
@@ -3,6 +3,8 @@
 bold=$(tput bold)
 italic="\e[3m"
 normal=$(tput sgr0)
+cyan="\033[0;36m"
+reset_color="\033[0m"
 
 lines=$(wc -l < "$todo_task_file")
 i=0
@@ -16,7 +18,7 @@ while [[ $i -lt $((lines)) ]]; do
   
   if [ "$task_status" = "[>" ]; then
     # The selected task should be bold
-    printf "${italic}${bold}    -> %s${normal}\n\n" "$task"  
+    printf "${cyan}${italic}${bold}    -> %s${normal}${reset_color}\n\n" "$task"  
   else
     # All other tasks should be normal
     printf "${normal}${padding}%s\n\n" "$task"
